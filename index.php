0822955b18a98038922bb10fde0c510f01b65050
Daniel Blankenship <danielblankenship@Daniels-MacBook-Air.local>
2016-09-23 18:29:35 -0600

    initializing project

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>Tutorial</title>
+   </head>
+   <body>
+      <h1>Tutorial</h1>
+      <p>
+         This is a tutorial page.
+      </p>
+   </body>
+   <script type="text/javascript" src="tutorial.js"></script>
+</html>
---
+++ tutorial.js
@@ -0,0 +1,10 @@
+var a = 1;
+
+function scope() {
+   var a = 5;
+   console.log(a);
+}
+
+scope();
+
+console.log(a);
